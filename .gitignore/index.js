838f72f119efa89865af7629554146001a9cbb19
DroleMalDaire <47672937+DroleMalDaire@users.noreply.github.com>
2019-04-24 18:27:37 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,8 +1,4 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const bot = new Discord.Client();
-var prefix = "?";
-const config = require('./config.json');
 
 client.login('NTcwNjM1MTkyODIyMzk4OTc2.XMCGmw.z_5rVt9ZvSqfduaU8ru2pCi62iI');
 
