66ffa8b792930b6e79cf0ba95c72984436dbf7ab
DroleMalDaire <47672937+DroleMalDaire@users.noreply.github.com>
2019-04-24 18:49:18 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 var bot = new Discord.Client();
-var prefix = "?";
 
 bot.on("ready", function() {
 	bot.user.setActivity('CandiceBOT, !help');
@@ -8,20 +7,3 @@ bot.on("ready", function() {
 })
 
 bot.login("NTcwNjM1MTkyODIyMzk4OTc2.XMCGmw.z_5rVt9ZvSqfduaU8ru2pCi62iI")
-
-client.on('message', message => {
-	if(message.content.startsWith(prefix + 'bill')) {
-		const embed = new Discord.RichEmbed()
-		.setFooter("demandé par " + message.author.username, message.author.avatarURL)
-		.setImage(ChapiNarkuss)
-		.setColor(0xE31212)
-		.setDescription(`ça c'est Bill, et son ami Billy 
-		Bill et Billy sont copains et ne s'insulte jamais 
-		Bill et Billy jouent à lol ensemble 
-		Bill et Billy sont les champions de France de lol et les champions du monde d'europe avec leurs équipes
-		Bill et Billy sont des copains 
-		Sois comme Bill et Billy`)
-		return message.channel.send({embed})
-		}
-}
-);
