891d462cfc2fa8a2be1166e928a9566a277fca34
DroleMalDaire <47672937+DroleMalDaire@users.noreply.github.com>
2019-04-24 18:39:18 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+var bot = new Discord.Client();
 
-bot.login('NTcwNjM1MTkyODIyMzk4OTc2.XMCGmw.z_5rVt9ZvSqfduaU8ru2pCi62iI');
+bot.on("ready", function() {
+	bot.user.setActivity('CandiceBOT, !help');
+	console.log("le bot a bien été connecté")
+})
 
-bot.on('ready', () => {
-	console.log(`Connetion effectuer avec ${client.user.tag} !`);
-});
+bot.login("NTcwNjM1MTkyODIyMzk4OTc2.XMCGmw.z_5rVt9ZvSqfduaU8ru2pCi62iI")
