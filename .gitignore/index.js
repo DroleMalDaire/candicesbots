93df82ff3f74eb582a9bf8e001de7416b4e1a5a3
DroleMalDaire <47672937+DroleMalDaire@users.noreply.github.com>
2019-04-24 18:29:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-client.login('NTcwNjM1MTkyODIyMzk4OTc2.XMCGmw.z_5rVt9ZvSqfduaU8ru2pCi62iI');
+bot.login('NTcwNjM1MTkyODIyMzk4OTc2.XMCGmw.z_5rVt9ZvSqfduaU8ru2pCi62iI');
 
-client.on('ready', () => {
+bot.on('ready', () => {
 	console.log(`Connetion effectuer avec ${client.user.tag} !`);
 });
